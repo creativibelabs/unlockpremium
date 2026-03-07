e320f41d1ee3113cbd634d3d899cae60fb8b7779
CreatiVibe Labs <147229393+creativibelabs@users.noreply.github.com>
2026-03-07 05:25:40 +0500

    Add additional script tag in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <head><script src="https://pl28861013.effectivegatecpm.com/64/c1/a3/64c1a34fab3f219cff6f4ceda2b80e62.js"></script></head>
+      <head>
+        <script src="https://pl28861013.effectivegatecpm.com/64/c1/a3/64c1a34fab3f219cff6f4ceda2b80e62.js"></script>
+        <script src="https://pl28861122.effectivegatecpm.com/54/63/8f/54638fd1140cfbd2f17cb7ab3f18852a.js"></script>
+      </head>
       <body
         className={`${sora.variable} antialiased min-h-screen bg-[#0a0a0f] text-white font-[family-name:var(--font-sora)]`}
       >
